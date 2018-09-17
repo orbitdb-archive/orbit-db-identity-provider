8e25b39e26883a0fa55799c757eabc7ccb87f90e
shamb0t <shams.methnani@gmail.com>
2018-09-17 17:03:13 +0400

    Make arguments optional
    Make keystore required
    
    Update dependencies

--- src/identity-provider.js
+++ src/identity-provider.js
@@ -11,7 +11,7 @@ class IdentityProvider {
     this._keystore = keystore
   }
 
-  async createIdentity(id, type, identitySignerFn) {
+  async createIdentity(id, options = {}) {
     // Get the key for id from the keystore or create one
     // if it doesn't exist
     const key = await this._keystore.getKey(id) ||
@@ -19,9 +19,9 @@ class IdentityProvider {
     // Sign with the key for the id
     const selfSigningFn = async (id, data) => await this._keystore.sign(key, data)
     // If no type was indicated, set to default
-    type = isDefined(type) ? type : defaultType
+    const type = isDefined(options.type) ? options.type : defaultType
     // If signing function was not passed, use keystore as the identity signer
-    identitySignerFn = isDefined(identitySignerFn) ? identitySignerFn : selfSigningFn
+    const identitySignerFn = isDefined(options.identitySignerFn) ? options.identitySignerFn : selfSigningFn
     // Sign the id with the signing key we're going to use
     const idSignature = await this._keystore.sign(key, id)
     // Get the hex string of the public key
@@ -31,9 +31,9 @@ class IdentityProvider {
     return new Identity(id, publicKey, idSignature, pubKeyIdSignature, type, this)
   }
 
-  static async createIdentity (keystore, id, type, identitySignerFn) {
+  static async createIdentity (keystore, id, options = {}) {
     const identityProvider = new IdentityProvider(keystore)
-    return await identityProvider.createIdentity(id, type, identitySignerFn)
+    return await identityProvider.createIdentity(id, options)
   }
 
   static async verifyIdentity (identity, verifierFunction) {
