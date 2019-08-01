b8b67fd63823e0c461d986a6d22b11f47fd8f8be
shamb0t <shams.methnani@gmail.com>
2019-08-01 15:51:34 +0100

    add keys to browser storage

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -13,16 +13,17 @@ if (!isNode) {
   const level = require('level-js')
   const storagePath = path.resolve('./test/fixtures/savedKeys')
   const signingStore = levelup(level(storagePath))
-  console.log(signingStore.db)
 
   let copyFixtures = []
   copyFixtures.push(signingStore.open())
 
+  const existingKey = require('./fixtures/keys/existing')
   const testKey1 = require('./fixtures/keys/QmPhnEjVkYE1Ym7F5MkRUfkD6NtuSptE7ugu1Ggr149W2X')
-  // const testKey2 = require('./fixtures/keys/0260baeaffa1de1e4135e5b395e0380563a622b9599d1b8e012a0f7603f516bdaa')
+  const testKey2 = require('./fixtures/keys/0260baeaffa1de1e4135e5b395e0380563a622b9599d1b8e012a0f7603f516bdaa')
 
-  copyFixtures.push(signingStore.put('035756c20f03ec494d07e8dd8456f67d6bd97ca175e6c4882435fe364392f13140', JSON.stringify(testKey1)))
-  // copyFixtures.push(signingStore.put('0260baeaffa1de1e4135e5b395e0380563a622b9599d1b8e012a0f7603f516bdaa', JSON.stringify(testKey1)))
+  copyFixtures.push(localStorage.setItem('existing', JSON.stringify(existingKey)))
+  copyFixtures.push(signingStore.put('QmPhnEjVkYE1Ym7F5MkRUfkD6NtuSptE7ugu1Ggr149W2X', JSON.stringify(testKey1)))
+  copyFixtures.push(signingStore.put('0260baeaffa1de1e4135e5b395e0380563a622b9599d1b8e012a0f7603f516bdaa', JSON.stringify(testKey2)))
 
   Promise.all(copyFixtures)
 }
