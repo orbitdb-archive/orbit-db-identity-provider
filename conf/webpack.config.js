3a14bbf33adf976a40d736a51d2a87f2bc2c0530
haad <haad@headbanggames.com>
2023-01-12 12:47:01 +0200

    Fix dist build, rename dist build output file

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,8 +1,10 @@
 import path from 'path'
 import webpack from 'webpack'
 import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
 
 export default (env, argv) => {
+  const require = createRequire(import.meta.url)
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
 
@@ -10,15 +12,21 @@ export default (env, argv) => {
     mode: 'production',
     entry: './src/identities.js',
     output: {
-      libraryTarget: 'var',
-      library: 'Identities',
-      filename: '../dist/orbit-db-identity-provider.min.js'
+      filename: '../dist/orbit-db-identity-provider.min.js',
+      library: {
+        name: 'Identities',
+        type: 'var',
+        export: 'default'
+      }
     },
     target: 'web',
     devtool: 'source-map',
     plugins: [
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer']
       })
     ],
     resolve: {
@@ -31,7 +39,8 @@ export default (env, argv) => {
       fallback: {
         assert: require.resolve('assert'),
         path: require.resolve('path-browserify'),
-        stream: require.resolve('stream-browserify')
+        stream: require.resolve('stream-browserify'),
+        fs: false
       }
     },
     resolveLoader: {
